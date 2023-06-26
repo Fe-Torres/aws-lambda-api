5095f58419721151bb5be974ed963a2b7e42566a
Felipe Torres <felipeost.torres@gmail.com>
2023-06-26 18:44:02 -0300

    fix: user test

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,3 @@
-
-
 export default {
   coverageProvider: 'v8',
-  testMatch: [
-  ],
 };
--- src/useCase/User/createUserUsecase/createUser.test.ts
+++ src/useCase/User/createUserUsecase/createUser.test.ts
@@ -1,8 +1,3 @@
-import { UserDTO } from 'src/model/user/interfaces/userDto';
-import { User } from 'src/model/user/User';
-import { UserRepositoryInMemory } from '../../../infra/repository/inMemory/UserRepositoryInMemory';
-import { CreateUserUseCase } from './createUser';
-
 describe('CreateUserUseCase', () => {
   // let createUserUseCase: CreateUserUseCase;
   // let userRepository: UserRepositoryInMemory;
@@ -11,22 +6,5 @@ describe('CreateUserUseCase', () => {
     // Create a new instance of the UserRepositoryMock for each test
   });
 
-  it.todo('should create a new user', async () => {
-    // Arrange
-    const userData: UserDTO = {
-      name: 'John Doe',
-      age: 25,
-      email: 'john@example.com',
-    };
-    const userRepository = new UserRepositoryInMemory();
-    const createUserUseCase = new CreateUserUseCase(userRepository);
-    // Act
-    const createdUser = await createUserUseCase.execute(userData);
-
-    // Assert
-    expect(createdUser).toBeInstanceOf(User);
-    expect(createdUser.name).toBe(userData.name);
-    expect(createdUser.age).toBe(userData.age);
-    expect(createdUser.email).toBe(userData.email);
-  });
+  it.todo('should create a new user');
 });
