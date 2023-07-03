ff1126dc9319faac8f5892df0817d58f2a0a6fcd
Felipe Torres <felipeost.torres@gmail.com>
2023-07-03 00:01:57 -0300

    feat: adding validation user email

--- src/infra/dynamoDb/repositories/userRepository.ts
+++ src/infra/dynamoDb/repositories/userRepository.ts
@@ -2,6 +2,8 @@ import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import { IUserRepository } from '@models/user/interfaces/IUserRepository';
 import { User } from '@models/user/User';
 import dynamoDBClient from '../client';
+import { UserDTO } from '../../../model/user/interfaces/userDto';
+import { UserMapperDynamoDb } from './helper/UserMapper';
 
 export class DynamoDBUserRepository implements IUserRepository {
   private readonly tableName: string;
@@ -10,6 +12,20 @@ export class DynamoDBUserRepository implements IUserRepository {
   constructor() {
     this.tableName = 'UserTable';
     this.documentClient = dynamoDBClient();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    const params: DocumentClient.GetItemInput = {
+      TableName: this.tableName,
+      Key: {
+        email: email,
+      },
+    };
+
+    const result = await this.documentClient.get(params).promise();
+    const user = UserMapperDynamoDb.mapGetResultToUser(result);
+
+    return user;
   }
 
   async save(user: User): Promise<User> {
@@ -25,12 +41,7 @@ export class DynamoDBUserRepository implements IUserRepository {
 
     await this.documentClient.put(params).promise();
 
-    return {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      age: user.age,
-    };
+    return user;
   }
 
   async findById(userID: string): Promise<User | null> {
@@ -42,16 +53,45 @@ export class DynamoDBUserRepository implements IUserRepository {
     };
 
     const result = await this.documentClient.get(params).promise();
+    const user = UserMapperDynamoDb.mapGetResultToUser(result);
 
-    if (!result.Item) {
+    return user;
+  }
+
+  async updateById(userID: string, dataToUpdate: UserDTO): Promise<User | null> {
+    const userToUpdate = await this.findById(userID);
+    if (!userToUpdate) {
       return null;
     }
 
-    return {
-      id: result.Item.id,
-      name: result.Item.name,
-      email: result.Item.email,
-      age: result.Item.age,
+    const params: DocumentClient.UpdateItemInput = {
+      TableName: this.tableName,
+      Key: {
+        id: userID,
+      },
+      UpdateExpression: 'set #name = :name, #email = :email, #age = :age',
+      ExpressionAttributeValues: {
+        ':name': dataToUpdate.name,
+        ':email': dataToUpdate.email,
+        ':age': dataToUpdate.age,
+      },
+      ReturnValues: 'ALL_NEW',
     };
+
+    const result = await this.documentClient.update(params).promise();
+    const updatedUser = UserMapperDynamoDb.mapGetResultToUser(result);
+
+    return updatedUser;
+  }
+
+  async deleteById(userID: string): Promise<void> {
+    const params: DocumentClient.DeleteItemInput = {
+      TableName: this.tableName,
+      Key: {
+        id: userID,
+      },
+    };
+
+    await this.documentClient.delete(params).promise();
   }
 }
--- src/model/user/interfaces/IUserRepository.ts
+++ src/model/user/interfaces/IUserRepository.ts
@@ -4,4 +4,7 @@ import { UserDTO } from './userDto';
 export interface IUserRepository {
   save(user: User): Promise<UserDTO>;
   findById(userID: string): Promise<UserDTO | null>;
+  findByEmail(email: string): Promise<UserDTO | null>;
+  updateById(userID: string, dataToUpdate: UserDTO): Promise<UserDTO | null>;
+  deleteById(userID: string): Promise<void>;
 }
